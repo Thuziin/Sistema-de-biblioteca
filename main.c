1963645586852f0fdc30887ad44b7cf0d9e5018e
Arthur Santos <arthuroliveira00132@gmail.com>
2023-11-17 20:44:40 -0300

    criação struct 'Livro', 'Usuario' e 'Reserva'

--- main.c
+++ main.c
@@ -5,11 +5,29 @@ typedef struct {
     char instituicao[150];
 } Autor;
 
-struct Livro {
+typedef struct {
     char identificador[150];
+    char titulo[150];
     char *autores[150];
-};
+    int ano;
+    int edicao;
+    char editora[50];
 
+}Livro;
+
+typedef struct {
+    char identificador[150];
+    char nome[150];
+    char endereço[250];
+    int telefone;
+} Usuario;
+
+typedef struct  {
+    char dataInicio[9];
+    char dataFim[9];
+    Usuario *identificadorUser;
+    Livro *identificadorLivro;
+} Reserva;
 
 
 int main () {
